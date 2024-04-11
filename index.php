c9885a7df6544db906c779edc9177544bf5b3c00
Maninderrz <139135206+Maninderrz@users.noreply.github.com>
2024-04-11 13:44:17 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>High School for Disabled and Special Needs Children</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<Section class="header">
+    <nav class="links">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="demo.php">Demo</a></li>
+            <li><a href="contact.html">Contact us</a></li>
+            <li><a href="resources.html">Resources</a></li>
+            <li><a href="about.html">About Dev</a></li>
+        </ul>
+    </nav>
+
+    <div class="heading">
+        <h1>Peel Special Secondary School</h1>
+        <p>Ontario's #1 school for chilren with special needs</p>
+    </div>
+    <footer>
+        <p>~Copyright to Peel Special Secondary School~</p>
+    </footer>
+</Section>
+
+</body>
+</html>
